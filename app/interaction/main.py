51c43fb0ea8918809388095a8f6d5b68740ce3b0
Bas Magré <bas.magre@topicus.nl>
2025-06-27 19:17:47 +0200

    updates

--- app/interaction/main.py
+++ app/interaction/main.py
@@ -1,6 +1,34 @@
 from PyQt6 import QtWidgets, QtGui, QtCore
 from generated.main import Ui_MainWindow
 from modules.k8s import K8s
+import time
+
+class ThreadResultListFunction(QtCore.QThread):
+    signal = QtCore.pyqtSignal(list)
+    def __init__(self, listReturnFunction):
+        self.listReturnFunction = listReturnFunction
+        super().__init__()
+
+    def run(self):
+        result = self.listReturnFunction()
+        self.signal.emit(result)
+
+class ThreadCheckJob(QtCore.QThread):
+    signal = QtCore.pyqtSignal(tuple)
+    def __init__(self, k8s: K8s, namespace: str, jobName: str):
+        self.k8s = k8s
+        self.namespace = namespace
+        self.jobName = jobName
+        super().__init__()
+
+    def run(self):
+        while True:
+            status = self.k8s.currentStatusJob(self.namespace, self.jobName)
+            self.signal.emit((self.jobName, status))
+            if status in ("Complete", "Failed"):
+                # We are done!
+                break
+            time.sleep(5)
 
 class IntUi_MainWindow(Ui_MainWindow):
     def __init__(self):
@@ -10,8 +38,7 @@ class IntUi_MainWindow(Ui_MainWindow):
         
         self.mainWindow.setFixedSize(self.mainWindow.size())
         self.mainWindow.show()
-        
-        self.k8s = K8s()
+
         self.currentNamespace = None
 
         self.namespacesModel = QtGui.QStandardItemModel()
@@ -23,20 +50,65 @@ class IntUi_MainWindow(Ui_MainWindow):
         self.listViewCronJobs.setModel(self.cronJobsModel)
         self.listViewCronJobs.setEditTriggers(QtWidgets.QAbstractItemView.EditTrigger.NoEditTriggers)
         
-        # todo background job
-        namespaces = self.k8s.getNamespaces()
+        self.jobsModel = QtGui.QStandardItemModel(0,2)
+        self.jobsModel.setHorizontalHeaderLabels(['Job', 'Status'])
+        self.tableViewJobs.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.ResizeMode.Stretch)
+        self.tableViewJobs.setModel(self.jobsModel)
+        self.tableViewJobs.setEditTriggers(QtWidgets.QAbstractItemView.EditTrigger.NoEditTriggers)
+        
+        self.pushButtonStartJob.clicked.connect(self.startJob)
+        self.StatusJobs = {}
+        self.threads = []
+
+        self.k8s = K8s()
+        self.getNamespaces()
+
+    def getNamespaces(self):
+        getNamespacesThread = ThreadResultListFunction(self.k8s.getNamespaces)
+        # connect trigger when thread is done
+        getNamespacesThread.signal.connect(self.putNamespacesOnScreen)
+        # start Thread
+        getNamespacesThread.start()
+        # put thread in current active class (keep it alive!)
+        self.threads.append(getNamespacesThread)
+    
+    def putNamespacesOnScreen(self, namespaces: list):
         for i in namespaces:
             self.namespacesModel.appendRow(QtGui.QStandardItem(i))
-            
-        self.pushButtonStartJob.clicked.connect(self.startJob)
-        
+    
     def getCronJobs(self, index):
+        self.listViewNamespaces.setDisabled(True)
+        self.cronJobsModel.clear()
         self.currentNamespace = self.namespacesModel.item(index.row()).text()
-        cronJobs = self.k8s.getCronJobs(self.currentNamespace)
+        getCronJobsThread = ThreadResultListFunction(lambda: self.k8s.getCronJobs(self.currentNamespace))
+        getCronJobsThread.signal.connect(self.putCronJobsOnScreen)
+        getCronJobsThread.start()
+        self.threads.append(getCronJobsThread)
+        self.listViewNamespaces.setDisabled(False)
+
+    def putCronJobsOnScreen(self, cronJobs: list):
         for i in cronJobs:
             self.cronJobsModel.appendRow(QtGui.QStandardItem(i))
-            
+
+    def updateStatusJob(self, jobStatus: tuple):
+        jobName = jobStatus[0]
+        status = jobStatus[1]
+        if jobName in self.StatusJobs:
+            statusItem = self.StatusJobs.get(jobName)
+            statusItem.setText(status)
+        else:
+            self.StatusJobs[jobName] = QtGui.QStandardItem(status)
+            self.jobsModel.appendRow([QtGui.QStandardItem(jobName), self.StatusJobs[jobName]])
+
+
     def startJob(self):
-        b = self.listViewCronJobs.currentIndex()
-        cronJobName = self.cronJobsModel.item(b.row()).text()
-        self.k8s.createJobFromCronJob(self.currentNamespace, cronJobName)
+        currentRow = self.listViewCronJobs.currentIndex().row()
+        if currentRow > 0:
+            self.pushButtonStartJob.setEnabled(False)
+            cronJobName = self.cronJobsModel.item(currentRow).text()
+            jobName = self.k8s.createJobFromCronJob(self.currentNamespace, cronJobName)
+            statusJob = ThreadCheckJob(self.k8s, self.currentNamespace, jobName)
+            statusJob.signal.connect(self.updateStatusJob)
+            statusJob.start()
+            self.threads.append(statusJob)
+            self.pushButtonStartJob.setEnabled(True)
--- app/modules/k8s.py
+++ app/modules/k8s.py
@@ -8,7 +8,7 @@ class K8s():
         self.corev1 = client.CoreV1Api()
         self.batchv1 = client.BatchV1Api()
 
-    def getNamespaces(self):
+    def getNamespaces(self) -> list:
         listNamespaces = self.corev1.list_namespace()
         namespaces = []
         for i in listNamespaces.items:
@@ -17,7 +17,7 @@ class K8s():
         namespaces.sort()
         return namespaces
 
-    def getCronJobs(self, namespace: str):
+    def getCronJobs(self, namespace: str) -> list:
         listNamespacedCronJob = self.batchv1.list_namespaced_cron_job(namespace=namespace)
         cronJobs = []
         for i in listNamespacedCronJob.items:
@@ -26,9 +26,33 @@ class K8s():
         cronJobs.sort()
         return cronJobs
     
-    def createJobFromCronJob(self, namespace: str, cronjob: str):
+    def createJobFromCronJob(self, namespace: str, cronjob: str) -> str:
         readNamespacedCronJob = self.batchv1.read_namespaced_cron_job(name=cronjob, namespace=namespace)
         randomString = ''.join(random.choices(string.ascii_lowercase + string.digits, k=5))
-        metadata = client.V1ObjectMeta(name="{0}-{1}".format(readNamespacedCronJob.metadata.name, randomString) )
-        body = client.V1Job(api_version="batch/v1", kind="Job", metadata=metadata, spec=readNamespacedCronJob.spec.job_template.spec)
-        return self.batchv1.create_namespaced_job(namespace=namespace, body=body)
+        jobName = "{0}-{1}".format(readNamespacedCronJob.metadata.name, randomString)
+        jobSpec = readNamespacedCronJob.spec.job_template.spec
+        jobSpec.ttl_seconds_after_finished = 120
+        metadata = client.V1ObjectMeta(name=jobName)
+        body = client.V1Job(api_version="batch/v1", kind="Job", metadata=metadata, spec=jobSpec)
+        self.batchv1.create_namespaced_job(namespace=namespace, body=body)
+        return jobName
+    
+    def currentStatusJob(self, namespace: str, jobName: str) -> str:
+        statusText = "-"
+        try:
+            job = self.batchv1.read_namespaced_job(name=jobName, namespace=namespace)
+            if hasattr(job.status, 'active') and job.status.active and job.status.active > 0:
+                a = "Active" # Running
+            if job.status.conditions:
+                for condition in job.status.conditions:
+                    if condition.status:
+                        if condition.type and job.status.succeeded > 0:
+                            statusText = condition.type # Complete
+                        else:
+                            statusText = "{0} - {1}".format(condition.type, condition.message) # Failed
+        except client.exceptions.ApiException as e:
+            if e.reason == "Not Found":
+                statusText = "Not Found"
+        except:
+            statusText = "Error getting Status!"
+        return statusText
